7f645b5196606bdd336eedd5d81ef733b62d6365
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-05 13:22:13 +0300

    feat: experience section

---
+++ src/components/Experience/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+import {BriefcaseIcon, CalendarIcon} from "@primer/octicons-react";
+
+function Experience() {
+    return (
+        <div className="experience-container">
+            <h4 className="title">Experience</h4>
+            <div className="experience">
+                <h3 className="title">Front-end Developer</h3>
+                <div className="company"><BriefcaseIcon size={16} fill="#737373FF" /> Freelance </div>
+                <div className="date-range"><CalendarIcon size={16} fill="#737373FF" /> Jan 2021 - Present</div>
+            </div>
+        </div>
+    );
+}
+
+export default Experience;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Introduction from "../components/Introduction";
+import Experience from "../components/Experience";
 
 function Home() {
     return (
         <div className={"app"}>
             <Navbar />
             <Introduction />
+            <Experience />
         </div>
     )
 }
