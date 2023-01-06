e688cd796f9d8563487cd8872ccba8df64c68b36
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-06 11:01:59 +0300

    feat: about section

---
+++ src/components/About/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+function About() {
+    return (
+        <div className="about-container">
+            <h4>About</h4>
+            <p className="paragraph">
+                My name is bedirhan, I am 19 years old. I am a high school student who continues his school life remotely.
+                I developed many web applications and web designs.
+                <br /> <br />
+                I have been dealing with this business since 2020 and I have developed many open source projects. I love to develop projects with up-to-date technologies, I like to learn more while doing.
+            </p>
+        </div>
+    );
+}
+
+export default About;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import Introduction from "../components/Introduction";
 import Experience from "../components/Experience";
+import About from "../components/About";
 
 function Home() {
     return (
         <div className={"app"}>
             <Navbar />
             <Introduction />
+            <About />
             <Experience />
         </div>
     )
