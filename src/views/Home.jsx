79f9a2a7ad3ffd54523499801f3e9fbb5ea08b72
bedirhandogan <bedirhandgn14@gmail.com>
2023-01-06 14:51:10 +0300

    feat: page loading text

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,25 @@
-import Navbar from "../components/Navbar";
-import Introduction from "../components/Introduction";
-import Experience from "../components/Experience";
-import About from "../components/About";
-import Works from "../components/Works";
-import Footer from "../components/Footer";
+import {lazy, Suspense} from "react";
+
+const Navbar = lazy(() => import('../components/Navbar'));
+const Introduction = lazy(() => import('../components/Introduction'));
+const Experience = lazy(() => import('../components/Experience'));
+const About = lazy(() => import('../components/About'));
+const Works = lazy(() => import('../components/Works'));
+const Footer = lazy(() => import('../components/Footer'));
 
 function Home() {
     return (
         <div className={"app"}>
-            <Navbar />
-            <Introduction />
-            <About />
-            <Experience />
-            <Works />
-            <Footer />
+            <Suspense fallback={<div className="loader-container">
+                Loading...
+            </div>}>
+                <Navbar />
+                <Introduction />
+                <About />
+                <Experience />
+                <Works />
+                <Footer />
+            </Suspense>
         </div>
     )
 }
