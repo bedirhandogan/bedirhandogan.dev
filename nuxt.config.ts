7ba455d6b879e14673754a9635d1dc42452c5e9c
bedirhandogan <bedirhandgn14@gmail.com>
2024-10-02 08:51:20 +0300

    refactor: some changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,15 @@ export default defineNuxtConfig({
                content:
                   'Bedirhan Doğan, Bedirhan, Doğan, personal website, personal, website, portfolio, blog, software, developer, software developer, software engineer, engineer, developer, programmer, programming, coding, code, software development, web development, web developer, web, development, developer, software, engineer, software engineer, software developer, developer, programmer, programming, coding, code, software development, web development, web developer, web, development, developer, software, engineer, software engineer, software developer, developer, programmer, programming, coding, code, software development, web development, web developer, web, development, developer, software, engineer, software engineer, software developer, developer, programmer, programming, coding, code, software development, web development, web developer, web, development, developer',
             },
+            {
+               name: 'description',
+               content:
+                  "Hi, I'm Bedirhan. I'm 21 years old and I got acquainted with the world of programming when I was 12. My interest in drawings eventually triggered my interest in front-end development and this hobby eventually turned into my job. I'm currently creating user-friendly and aesthetic web applications by combining design and coding.",
+            },
+            {
+               name: 'author',
+               content: 'Bedirhan Doğan',
+            },
          ],
       },
    },
